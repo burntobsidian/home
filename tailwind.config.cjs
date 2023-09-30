0e10de0b3d8c685bcef0ee1b2bfe3df5ed8045ca
burntobsidian <me@burntobsidian.com>
2023-10-01 02:34:04 +0530

    add cal sans font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				cal: ["Cal Sans", ...fontFamily.serif],
+				sans: ["Inter", ...fontFamily.sans],
+				mono: ["Roboto Mono", "monospace"],
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
 };
